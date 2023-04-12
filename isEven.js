e3316dd56ced3d1d97cdae64a086cebb532a5427
Alex Thornberry <alex.thornberry@newlook.com>
2023-04-12 21:46:52 +0100

    Made isEven as its own file

---
+++ isEven.js
@@ -0,0 +1,16 @@
+const isEven = (num) => {
+    if(num < 0){
+      num *= -1
+    }
+    if(num == 0 ){
+      return true
+    }
+    else if(num == 1 ){
+      return false
+    }
+    else {
+      return isEven(num-2)
+    }
+  }
+  
+  module.exports = isEven;
