bb56a9b4fbffb9cdefcd7f3e4c2c0e72bde172b2
Alex Thornberry <alex.thornberry@newlook.com>
2023-04-16 02:24:02 +0100

    adding negative number and 0 tests

--- isEven.test.js
+++ isEven.test.js
@@ -7,3 +7,15 @@ test('checks that 2 is an even number', () => {
 test('checks that 3 is not an even number', () => {
     expect(isEven(3)).toBe(false);
 }) 
+
+test('checks if negative 2 is an even number', () => {
+    expect(isEven(-2)).toBe(true);
+})
+
+test('checks if negative 3 is not an even number', () => {
+    expect(isEven(-3)).toBe(false);
+})
+
+test('checks if 0 is an even number', () => {
+    expect(isEven(0)).toBe(true);
+})
