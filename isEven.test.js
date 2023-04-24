fbb7fbcc2ed6f2fdabda9a0a39dff46991bdc620
Alex Thornberry <alex.thornberry@newlook.com>
2023-04-24 10:52:54 +0100

    isEven tests

--- isEven.test.js
+++ isEven.test.js
@@ -1,21 +1,24 @@
 const isEven = require('./isEven')
 
-test('checks that 2 is an even number', () => {
-    expect(isEven(2)).toBe(true);
-}) 
+describe('isEven', () => {
 
-test('checks that 3 is not an even number', () => {
-    expect(isEven(3)).toBe(false);
-}) 
+    it('checks that 2 is an even number', () => {
+        expect(isEven(2)).toBe(true);
+    }) 
 
-test('checks if negative 2 is an even number', () => {
-    expect(isEven(-2)).toBe(true);
-})
+    it('checks that 3 is not an even number', () => {
+        expect(isEven(3)).toBe(false);
+    }) 
 
-test('checks if negative 3 is not an even number', () => {
-    expect(isEven(-3)).toBe(false);
-})
+    it('checks if negative 2 is an even number', () => {
+        expect(isEven(-2)).toBe(true);
+    })
 
-test('checks if 0 is an even number', () => {
-    expect(isEven(0)).toBe(true);
+    it('checks if negative 3 is not an even number', () => {
+        expect(isEven(-3)).toBe(false);
+    })
+
+    it('checks if 0 is an even number', () => {
+        expect(isEven(0)).toBe(true);
+    })
 })
